385b89bc5547f1854b237cd93cf606596f60bc7e
Willian <front.willian@gmail.com>
2024-08-13 20:38:45 -0300

    Ajustes no global.js

--- app/scripts/global.js
+++ app/scripts/global.js
@@ -1,4 +1,21 @@
 import { navTemplate, openNavbar } from '../pages/nav/nav.js';
 
+function expandedLabProjects() {
+
+    let btnExpand = document.getElementById('labProjectsItemsExpand');
+    let projectItems = document.getElementById('labProjectsItems');
+
+    if (projectItems.classList.contains('expanded')) {
+        projectItems.classList.remove('expanded');
+        btnExpand.textContent = 'Expandir Projetos';
+
+    } else {
+        projectItems.classList.add('expanded');
+        btnExpand.textContent = 'Recolher Projetos';
+    }
+}
+
 navTemplate();
+
 window.openNavbar = openNavbar;
+window.expandedLabProjects = expandedLabProjects;
