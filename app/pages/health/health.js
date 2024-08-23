956a2a5ae87526e019fb4b47b746dac9634e29d4
Willian <front.willian@gmail.com>
2024-08-22 21:21:01 -0300

    FEATURE: Página de serviços e contatos

---
+++ app/pages/health/health.js
@@ -0,0 +1,8 @@
+import { navTemplate, openServiceList, openMenuMobile } from '../nav/nav.js';
+import { footerTemplate } from '../footer/footer.js';
+
+navTemplate();
+footerTemplate();
+
+window.openServiceList = openServiceList;
+window.openMenuMobile = openMenuMobile;
