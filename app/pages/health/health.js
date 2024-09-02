f5b806a20ab3180fdb41bf7ee88419fb2f0b483a
Willian <front.willian@gmail.com>
2024-09-01 21:16:19 -0300

    Uso do darkmode em todos os componentes.

--- app/pages/health/health.js
+++ app/pages/health/health.js
@@ -1,8 +1,14 @@
-import { navTemplate, openServiceList, openMenuMobile } from '../nav/nav.js';
+import { navTemplate, openServiceList, openMenuMobile, detectedThemeColor, themeColor } from '../nav/nav.js';
 import { footerTemplate } from '../footer/footer.js';
 
-navTemplate();
-footerTemplate();
+document.addEventListener('DOMContentLoaded', async function () {
+    await navTemplate();
+    detectedThemeColor();
+    themeColor();
+    footerTemplate();
 
-window.openServiceList = openServiceList;
-window.openMenuMobile = openMenuMobile;
+    window.openServiceList = openServiceList;
+    window.openMenuMobile = openMenuMobile;
+    window.detectedThemeColor = detectedThemeColor;
+    window.themeColor = themeColor;
+});
