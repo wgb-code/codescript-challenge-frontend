594c69d9b6932e6741d7957e847c3980a5e6bab3
Willian Gabriel <163947559+wgb-code@users.noreply.github.com>
2024-10-22 14:17:27 -0300

    Update services.js

---
+++ app/pages/services/services.js
@@ -0,0 +1,9 @@
+import { navbarTemplate } from "../../components/nav/nav.js";
+import { footerTemplate } from "../../components/footer/footer.js";
+import { scrollReveal } from "../../services/scrollReveal.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+    navbarTemplate();
+    footerTemplate();
+    scrollReveal();
+});
